e52e3d68b3225d4b06832ab0a08bb45a55f81c27
Jakob Knigga <ohmyjakob@gmail.com>
2017-07-01 14:25:37 -0400

    Create eth-price.userscript.js
    inital commit. no content.

---
+++ eth-price.userscript.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Ethereum Pricer
+// @namespace    https://github.com/jknigga/eth-miner-price
+// @version      0.1
+// @description  Shows the price of Ethereum
+// @author       Jakob Knigga
+// @match        https://ethermine.org/miners/*
+// @require      https://code.jquery.com/jquery-2.2.4.min.js
+// @updateURL    https://raw.githubusercontent.com/jknigga/eth-miner-price/master/eth-price.userscript.js
+// @grant        none
+// ==/UserScript==
+
+(function ($, undefined) {
+    $(function () {
+        $(document).ready(function(){
+           // do the things!
+        });
+    });
+})(window.jQuery.noConflict(true));
