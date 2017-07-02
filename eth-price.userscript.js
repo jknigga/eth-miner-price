20ef532148ec2e7dde37176fe77c914352aa2c45
Jakob Knigga <ohmyjakob@gmail.com>
2017-07-01 22:52:37 -0400

    Update eth-price.userscript.js

--- eth-price.userscript.js
+++ eth-price.userscript.js
@@ -1,34 +1,32 @@
 // ==UserScript==
 // @name         Ethereum Pricer
 // @namespace    https://github.com/jknigga/eth-miner-price
-// @version      0.1.3
+// @version      0.1.4
 // @description  Shows the price of Ethereum
 // @author       Jakob Knigga
 // @match        https://ethermine.org/miners/*
 // @require      https://code.jquery.com/jquery-2.2.4.min.js
 // @require      http://courses.ischool.berkeley.edu/i290-4/f09/resources/gm_jq_xhr.js
 // @updateURL    https://raw.githubusercontent.com/jknigga/eth-miner-price/master/eth-price.userscript.js
-// @grant        none
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
 (function ($, undefined) {
     $(function () {
         var eth_data;
+
         $(document).ready(function(){
             //$.getJSON('https://coinmarketcap-nexuist.rhcloud.com/api/eth', function (data) {
             //    eth_data = data;
             //});
-           
             GM_xmlhttpRequest({
              method: "GET",
              url: "https://coinmarketcap-nexuist.rhcloud.com/api/eth",
              onload: function(xhr) {
               var data = eval("(" + xhr.responseText + ")");
-              eth_data = data;
+              console.log(data.price.usd);
             }
           });
-            
-            console.log(eth_data.price.usd);
         });
     });
 })(window.jQuery.noConflict(true));
