59e9efe06847459b10d8137e3bb9fd5edf728713
Jakob Knigga <ohmyjakob@gmail.com>
2017-07-01 22:31:07 -0400

    Update eth-price.userscript.js

--- eth-price.userscript.js
+++ eth-price.userscript.js
@@ -6,6 +6,7 @@
 // @author       Jakob Knigga
 // @match        https://ethermine.org/miners/*
 // @require      https://code.jquery.com/jquery-2.2.4.min.js
+// @require      http://courses.ischool.berkeley.edu/i290-4/f09/resources/gm_jq_xhr.js
 // @updateURL    https://raw.githubusercontent.com/jknigga/eth-miner-price/master/eth-price.userscript.js
 // @grant        none
 // ==/UserScript==
